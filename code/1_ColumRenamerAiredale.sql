f2ce0f6b8f6f5d95fcc5b6b93182a9540e318875
John Birkinshaw <53085254+JohnDomar@users.noreply.github.com>
2024-04-24 09:28:19 +0100

    Add files via upload

---
+++ code/1_ColumRenamerAiredale.sql
@@ -0,0 +1,66 @@
+﻿use [CB_MYSPACE_PAA]
+--1_ColumRenamerAiredale.sql
+
+IF OBJECT_ID('dbo.tmp_renamer', 'U') IS NOT NULL 
+ DROP TABLE dbo.tmp_renamer; 
+
+-- custom for Airedale updates
+
+select table_name
+, column_name 
+,lower(replace(column_name,'&','and')) as Newname
+ into tmp_renamer
+from INFORMATION_SCHEMA.COLUMNS
+where table_name like '%BIB%'
+order by table_name, column_name
+
+
+-- This just does a general replace on the newname
+
+  update tmp_renamer
+  set Newname = replace(Newname,'&','and') 
+
+  update [tmp_renamer]
+  set Newname = replace(Newname,'(','') 
+
+   update [tmp_renamer]
+  set Newname = replace(Newname,')','') 
+
+    update [tmp_renamer]
+  set Newname = replace(Newname,'_-_','_') 
+
+
+      update [tmp_renamer]
+  set Newname = replace(Newname,'-','') 
+
+      update [tmp_renamer]
+  set Newname = replace(Newname,' ','_') 
+
+
+        update [tmp_renamer]
+  set Newname = replace(Newname,'__','_') 
+
+          update [tmp_renamer]
+  set Newname = replace(Newname,'__','_') 
+
+        update [tmp_renamer]
+  set Newname = replace(Newname,'"','') 
+
+  
+  update tmp_renamer 
+  set Newname = case when isnumeric(left(Newname,1)) = '1' 
+	  then 'a'+ Newname else Newname end 
+
+-- renames the nhs numbers to digests - needs validation	  
+update tmp_renamer
+set Newname = REPLACE(Newname,'nhs','digest') 
+where (column_name like 'NHS_N%'
+or column_name like 'NHSN%')
+and column_name not like '%st%'
+
+
+update tmp_renamer
+set Newname = REPLACE(Newname,'idpatient','digest') 
+where (column_name like 'idpatient')
+
+select * from tmp_renamer
---
+++ code/2_ColumnRenamerExecutor.sql
@@ -0,0 +1,39 @@
+﻿use [CB_MYSPACE_PAA]
+
+--Renames tables bnased on tmp_renamer
+-- 2_ColumnRenamerExecutor.sql 
+
+declare @dbsx varchar(125)
+declare @fromx varchar(125)
+declare @tox varchar(125)
+
+declare @sql varchar(255)
+
+--wraps the new col name in square brackets
+--update tmp_renamer
+--set Newname = '['+Newname+']'
+
+
+ declare tablex cursor for 
+select table_name, column_name , Newname from tmp_renamer
+  where Newname != column_name collate latin1_general_cs_as
+
+ -- START OF THE CURSOR --
+
+open tablex
+fetch next from tablex into @dbsx,@fromx, @tox
+
+while (@@fetch_status = 0) 
+begin
+
+declare @sql1 nvarchar(400)
+set @sql1 = 'Exec sp_rename ''' +  @dbsx + '.['+ @fromx +']'',''' + @tox +''',''column'';' ;
+exec (@sql1);
+ print @sql1
+
+ fetch next from tablex into @dbsx,@fromx, @tox
+
+end
+
+close tablex
+deallocate tablex
